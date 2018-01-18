1549d332716881988e34a95aeee14c09f10bb742
Demo User <demouser@wc125-dhcp-206.brynmawr.edu>
2018-01-18 15:07:58 -0500

    Navigation and multi-screens!

--- blackjack/app/src/main/java/com/example/sellenburg/blackjack/MainActivity.java
+++ blackjack/app/src/main/java/com/example/sellenburg/blackjack/MainActivity.java
@@ -4,9 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     // this is the home screen, decides which game to play
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     Button single;
     Button two;
@@ -20,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         single = findViewById(R.id.single);
         single.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                singleMode(v);
             };
         });
 
@@ -28,8 +32,24 @@ public class MainActivity extends AppCompatActivity {
         two = findViewById(R.id.two);
         two.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                twoMode(v);
             };
         });
+    }
+
+    // Called when user taps single-player mode
+    public void singleMode(View view) {
+        Intent intent = new Intent(this, singlePlayer.class);
+        //String text = "YOU ARE IN SINGLE PLAYER MODE";
+        //intent.putExtra(text);
+        startActivity(intent);
+    }
+
+    // Called when user taps two-payer mode
+    public void twoMode(View view) {
+        Intent intent = new Intent(this, twoPlayer.class);
+        //String text = "YOU ARE IN TWO PLAYER MODE";
+        //intent.putExtra(text);
+        startActivity(intent);
     }
 }
