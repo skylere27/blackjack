771a660158bf83d992f28786e6edbf7ca2d25cd4
Demo User <demouser@wc125-dhcp-206.brynmawr.edu>
2018-01-18 12:35:07 -0500

    changed app name

--- blackjack/app/src/main/java/com/example/sellenburg/blackjack/MainActivity.java
+++ blackjack/app/src/main/java/com/example/sellenburg/blackjack/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    // this is the home screen, decides which game to play
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
