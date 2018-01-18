8761b13a967eee5bc6e28e07732f017ab4ff9e73
Demo User <demouser@wc125-dhcp-206.brynmawr.edu>
2018-01-18 14:42:06 -0500

    Added buttons, next is screen

--- blackjack/app/src/main/java/com/example/sellenburg/blackjack/MainActivity.java
+++ blackjack/app/src/main/java/com/example/sellenburg/blackjack/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.sellenburg.blackjack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     // this is the home screen, decides which game to play
+
+    Button single;
+    Button two;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // access solo mode
+        single = findViewById(R.id.single);
+        single.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            };
+        });
+
+        // access 2-player mode
+        two = findViewById(R.id.two);
+        two.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            };
+        });
     }
 }
